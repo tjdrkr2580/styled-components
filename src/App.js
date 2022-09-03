f0605cc1402fed07b3c71442081f4fcc96178bfd
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-09-04 02:47:49 +0900

    cra & circle

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background-image: black;
+  border-radius: 50%;
+`;
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
